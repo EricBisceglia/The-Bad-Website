88b093b6aac3dd24bc0b75bd3d1c7a9922c49543
Éric Bisceglia <bisceglia.eric@gmail.com>
2025-02-16 22:14:13 +0100

    Temporary index page

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ $page_lang        = array('FR', 'EN');
 $page_url         = "index";
 $page_description = "Hi, this is Bad, and you're on the wrong website.";
 
+// Hide header and footer except in local dev mode
+if($GLOBALS['website_url'] !== 'http://127.0.0.1/thebadwebsite/')
+{
+  $hide_header = true;
+  $hide_footer = true;
+}
+
 
 
 
@@ -22,9 +29,23 @@ $page_description = "Hi, this is Bad, and you're on the wrong website.";
 
 <div class="width_50">
 
-  <h1 class="padding_top">
-    <?=__('home_intro_title')?>
+  <h1 class="gigapadding_top bigpadding_bot align_center">
+    <?=__link('https://bsky.app/profile/thebad.website', __('home_intro_title'), 'text_light', is_internal: false)?>
   </h1>
+
+  <p class="align_center big">
+    <?=__link('https://bsky.app/profile/thebad.website', __('home_intro_text'), 'text_light', is_internal: false)?>
+  </p>
+
+  <p class="bigpadding_top align_center big">
+    <?=__link('https://bsky.app/profile/thebad.website', __('home_intro_bsky'), 'text_light', is_internal: false)?>
+  </p>
+
+  <p class="align_center hugepadding_top">
+    <a href="https://bsky.app/profile/thebad.website">
+      <img style="height: 6.0rem; width: 6.0rem;" src="<?=$path?>img/icons/bluesky_page.png" alt="Bluesky" title="Bluesky">
+    </a>
+  </p>
 
 </div>
 
--- lang/main.lang.php
+++ lang/main.lang.php
@@ -15,3 +15,7 @@ if(substr(dirname(__FILE__),-8).basename(__FILE__) === str_replace("/","\\",subs
 // Header
 ___('home_intro_title', 'EN', "The Bad Website");
 ___('home_intro_title', 'FR', "Le mauvais site");
+___('home_intro_text',  'EN', "Come back in a few weeks :)");
+___('home_intro_text',  'FR', "Revenez dans quelques semaines :)");
+___('home_intro_bsky',  'EN', "In the meantime, you can find my smuggies <span class=\"text_blue\">on Bluesky</span>");
+___('home_intro_bsky',  'FR', "En attendant, vous pouvez trouver mes arroganteries <span class=\"text_blue\">sur Bluesky</span>");
